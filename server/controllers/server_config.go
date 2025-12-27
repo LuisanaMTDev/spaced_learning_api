2c2508233c9d0b48d0c58a72821c0c575a73340a
LuisanaMTDev <luisanamartineztorres25@gmail.com>
2025-12-27 18:21:53 -0400

    refactor: move server config setup to a single file

--- server/controllers/server_config.go
+++ server/controllers/server_config.go
@@ -1,16 +1,56 @@
 package controllers
 
-import "github.com/LuisanaMTDev/spaced_learning/server/database/gosql_queries"
+import (
+	"database/sql"
+	"os"
+
+	"github.com/LuisanaMTDev/spaced_learning/server/database/gosql_queries"
+	"github.com/rs/zerolog/log"
+	"golang.org/x/oauth2"
+)
 
 // This struct will contain the server config, as its name says, and the controllers.
 type ServerConfig struct {
-	DBQueries *gosql_queries.Queries
-	Platform  string
+	DBQueries   *gosql_queries.Queries
+	Platform    string
+	OAuthConfig oauth2.Config
 }
 
-func NewServerConfig(dbQueries *gosql_queries.Queries, runningPlatform string) *ServerConfig {
+func NewServerConfig() *ServerConfig {
+
+	platform := os.Getenv("PLATFORM")
+	var db *sql.DB
+	var err error
+
+	if platform == "PROD" {
+		dbURL := os.Getenv("DB_URL_PROD")
+		db, err = sql.Open("libsql", dbURL)
+		if err != nil {
+			log.Fatal().AnErr("error", err).Msg("ERROR: while opening db.")
+			return nil
+		}
+	} else {
+		dbURL := os.Getenv("DB_URL_DEV")
+		db, err = sql.Open("sqlite", dbURL)
+		if err != nil {
+			log.Fatal().AnErr("error", err).Msg("ERROR: while opening db.")
+			return nil
+		}
+	}
+
+	dbQueries := gosql_queries.New(db)
 	return &ServerConfig{
 		DBQueries: dbQueries,
-		Platform:  runningPlatform,
+		Platform:  platform,
+		OAuthConfig: oauth2.Config{
+			ClientID:     os.Getenv("CLIENT_ID"),
+			ClientSecret: os.Getenv("CLIENT_SECRET"),
+			Endpoint: oauth2.Endpoint{
+				AuthURL:  "https://github.com/login/oauth/authorize",
+				TokenURL: "https://github.com/login/oauth/access_token",
+			},
+			RedirectURL: "http://localhost:8090/oauth2/callback",
+			Scopes:      []string{"user"},
+		},
 	}
 }
--- server/main.go
+++ server/main.go
@@ -1,14 +1,9 @@
 package main
 
 import (
-	"database/sql"
 	"net/http"
-	"os"
-	"strings"
 
 	"github.com/LuisanaMTDev/spaced_learning/server/controllers"
-	"github.com/LuisanaMTDev/spaced_learning/server/database/gosql_queries"
-	"github.com/LuisanaMTDev/spaced_learning/server/frontend/views"
 	"github.com/joho/godotenv"
 	// "github.com/justinas/alice"
 	"github.com/rs/zerolog"
@@ -17,32 +12,11 @@ import (
 )
 
 func main() {
-
 	godotenv.Load()
 	zerolog.TimeFieldFormat = zerolog.TimeFormatUnix
-	platform := os.Getenv("PLATFORM")
-	var db *sql.DB
-	var err error
 
-	if platform == "PROD" {
-		dbURL := os.Getenv("DB_URL_PROD")
-		db, err = sql.Open("libsql", dbURL)
-		if err != nil {
-			log.Fatal().AnErr("error", err).Msg("ERROR: while opening db.")
-			return
-		}
-	} else {
-		dbURL := os.Getenv("DB_URL_DEV")
-		db, err = sql.Open("sqlite", dbURL)
-		if err != nil {
-			log.Fatal().AnErr("error", err).Msg("ERROR: while opening db.")
-			return
-		}
-	}
-
-	dbQueries := gosql_queries.New(db)
 	handler := http.NewServeMux()
-	serverConfig := controllers.NewServerConfig(dbQueries, platform)
+	serverConfig := controllers.NewServerConfig()
 
 	//End points
 	handler.Handle("GET /app/", http.StripPrefix("/app/", http.FileServer(http.Dir("./frontend/assets/"))))
@@ -67,6 +41,6 @@ func main() {
 
 	server := http.Server{Handler: handler, Addr: ":8090"}
 	log.Info().Str("running_platfotm", serverConfig.Platform).Msg("Running...")
-	err = server.ListenAndServe()
+	err := server.ListenAndServe()
 	log.Fatal().AnErr("server_error", err)
 }
